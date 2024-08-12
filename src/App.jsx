0b97d1fa0b549fcc48c327d2cdc568f0c799c638
gooncalvesgaby <gabrielle.goncalves@outlook.com.br>
2024-08-12 00:02:28 -0300

    organizando pastas e style components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
+import { createGlobalStyle } from "styled-components"
+import Button from "./Components/Buttons/button.jsx"
+
+const GlobalStyle = createGlobalStyle`
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+`
 function App() {
 
 
   return (
     <>
+    <GlobalStyle/>
+    <Button/>
     </>
   )
 }
---
+++ src/Components/Buttons/button.jsx
@@ -0,0 +1,11 @@
+import * as S from "./buttonStyle.jsx"
+
+function Button() {
+    return(
+        <S.Div>
+            
+        </S.Div>
+    )
+}
+
+export default Button
