66fed806e28e99b16f903b0498533ee1d6d28693
gooncalvesgaby <gabrielle.goncalves@outlook.com.br>
2024-09-05 15:03:37 -0300

    ajustes

--- src/Components/Buttons/button.jsx
+++ src/Components/Buttons/button.jsx
@@ -3,7 +3,9 @@ import * as S from "./buttonStyle.jsx"
 function Button() {
     return(
         <S.Div>
-            
+            <S.Button>
+                clique aqui para mudar o texto
+            </S.Button>
         </S.Div>
     )
 }
--- src/Components/Buttons/buttonStyle.jsx
+++ src/Components/Buttons/buttonStyle.jsx
@@ -1,5 +1,18 @@
 import styled from "styled-components"
 
 export const Div = styled.div`
+display: flex;
+justify-content: center;
+align-items: center;
+background-color: #b1b1b1;
+height: 100vh;
+`
 
+export const Button = styled.button`
+width: 15vw;
+height: 5vh;
+border: solid 1px black;
+border-radius: 1rem;
+color: white;
+background-color: #0c0303;
 `
